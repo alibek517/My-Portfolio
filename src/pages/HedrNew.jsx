db2dde3389848583da78cafa89c8264d9844e746
alibek517 <alibekbunyod@gmail.com>
2025-06-05 11:24:09 +0500

    koijhg

--- src/pages/HedrNew.jsx
+++ src/pages/HedrNew.jsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
 import React from 'react';
-import './style/Minhed.css'; // styling uchun
+import './style/HedrNew.css'; // styling uchun
 
 function Minhed() {
   return (
